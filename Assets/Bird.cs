39646bf9606aaa751fd30665c0aa4d6b1000783e
salmanarrahman <salmanarrahman@gmail.com>
2023-11-04 13:10:25 +0600

    whole project added

---
+++ Assets/Bird.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+
+    private bool isDead = false;
+    private Rigidbody2D rb2d;
+
+    public float upforce = 200f;
+
+    private Animator anim;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        rb2d.velocity = Vector2.zero;
+        isDead = true;
+        anim.SetTrigger("Die");
+        GameControl.instance.BirdDied();
+    }
+
+    // Use this for initialization
+    void Start () {
+
+        rb2d = GetComponent<Rigidbody2D> ();
+        anim = GetComponent<Animator>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (isDead == false) {
+            if (Input.GetMouseButtonDown(0)) {
+
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2(0,upforce));
+                anim.SetTrigger("Flap");
+
+            }
+        }
+
+     
+
+        
+		
+	}
+
+
+}
---
+++ Assets/ColumnPool.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColumnPool : MonoBehaviour {
+
+    public int columnPoilSize = 5;
+    public GameObject columnPrefab;
+    public float spawnRate = 4f;
+    public float columnMin = -1f;
+    public float columnMax = 3.5f;
+
+    private GameObject[] columns;
+    private Vector2 objectPoolPosition = new Vector2(-15f, -25f);
+    private float spawnXPosition = 10f;
+    private float timeScienceLastRespond;
+    private int currentColumn = 0;
+
+	// Use this for initialization
+	void Start () {
+
+        columns = new GameObject[columnPoilSize];
+        for (int i = 0; i < columnPoilSize;i++) {
+            columns[i] =  (GameObject)Instantiate(columnPrefab, objectPoolPosition, Quaternion.identity);
+        }
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        timeScienceLastRespond += Time.deltaTime;
+        if (GameControl.instance.gameOver == false && timeScienceLastRespond >= spawnRate)
+        {
+            timeScienceLastRespond = 0;
+            float spawnYPosition = Random.Range(columnMin,columnMax);
+            columns[currentColumn].transform.position = new Vector2(spawnXPosition,spawnYPosition);
+            currentColumn++;
+            if (currentColumn >= columnPoilSize) {
+                currentColumn = 0;
+            }
+
+        }
+		
+	}
+}
